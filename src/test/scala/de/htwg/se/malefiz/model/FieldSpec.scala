62786f0ce29da405d81282f53a22eba324cecb98
Robert Forsh <55754729+RF196@users.noreply.github.com>
2020-05-20 12:59:56 +0200

    Test FielSpec.scala

---
+++ src/test/scala/de/htwg/se/malefiz/model/FieldSpec.scala
@@ -0,0 +1,34 @@
+package de.htwg.se.malefiz.model
+
+import org.scalatest.{Matchers, WordSpec}
+
+class FieldSpec extends WordSpec with Matchers {
+
+  "A Field" when {
+    "not set to any Value" should {
+      val emptyField = Field(0, 0, destination = false, wallPermission = false, Point(0,0), hasWall = false)
+      "have Value 1 " in {
+        emptyField.playerNumber should be(0)
+      }
+      "have Value 0" in {
+        emptyField.playerNumber should be(0)
+      }
+      "have value false" in {
+        emptyField.destination should be(false)
+      }
+      "have value false" in {
+        emptyField.wallPermission should be(false)
+      }
+      "have value x = 0" in {
+        emptyField.coordinates.x should be(0)
+      }
+      "have value y = 0" in {
+        emptyField.coordinates.y should be(0)
+      }
+    }
+    "set to a specific value" should{
+      val nonEmptyField = 
+    }
+  }
+
+}
