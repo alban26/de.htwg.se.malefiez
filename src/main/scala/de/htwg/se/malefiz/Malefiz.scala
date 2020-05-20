7ffef465a17bc4d2764972386e8a7006b95b8823
Robert Forsh <55754729+RF196@users.noreply.github.com>
2020-05-20 10:06:46 +0200

    Class Field

--- src/main/scala/de/htwg/se/malefiz/Malefiz.scala
+++ src/main/scala/de/htwg/se/malefiz/Malefiz.scala
@@ -1,6 +1,6 @@
 package de.htwg.se.malefiz
 
-import de.htwg.se.malefiz.model.{Gameboard, Player}
+import de.htwg.se.malefiz.model.{Field, Gameboard, Player, Point}
 
 object Malefiz {
   def main(args: Array[String]): Unit = {
@@ -11,6 +11,17 @@ object Malefiz {
     println(gb.buildField(0))
     println("Robert")
     print("MergeTest")
+
+    var  gamefield = new Array[Field](100)
+
+    for (Int i <- 1 to 132) {
+       if (i == 1) {
+         gamefield[i] = Field(1, 0, true, false, Point(0,0), false)
+         println("                           (-)                           /n")
+       }
+
+    }
+
   }
 
 
---
+++ src/main/scala/de/htwg/se/malefiz/model/Field.scala
@@ -0,0 +1,15 @@
+package de.htwg.se.malefiz.model
+
+case class Field(fieldNumber: Int, playerNumber: Int, destination: Boolean,
+                 wallPermission: Boolean, coordinates: Point, hasWall: Boolean) {
+
+  def getFieldNumber : Int = this.fieldNumber;
+  def getPlayNumber : Int = this.playerNumber;
+  def isDestination: Boolean = this.destination;
+  def getWallPermission : Boolean = this.wallPermission
+  def getXCoordinate: Int = this.coordinates.x
+  def getYCoordinate: Int = this.coordinates.y
+
+
+}
+
