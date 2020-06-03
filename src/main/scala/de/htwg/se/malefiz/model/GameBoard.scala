851748861394bfd3062993a92f66ee647f7613a6
alban26 <alban.jashari@htwg-konstanz.de>
2020-06-03 18:40:55 +0200

    new changes

---
+++ src/main/scala/de/htwg/se/malefiz/model/GameBoard.scala
@@ -0,0 +1,150 @@
+package de.htwg.se.malefiz.model
+
+import scala.collection.mutable.Map
+import scala.io.Source
+
+case class GameBoard() {
+
+  def getCellList(filename: String): List[Cell] = {
+    val list = Source.fromFile(filename)
+    val inputData =  list.getLines
+      .map(line => line.split(" "))
+      .map{case Array(cellNumber, playerNumber, destination, wallPermission, hasWall, x, y) =>
+        Cell(cellNumber.toInt,
+          playerNumber.toInt,
+          destination.toBoolean,
+          wallPermission.toBoolean,
+          hasWall.toBoolean,
+          Point(x.toInt, y.toInt))}
+      .toList
+    list.close()
+    inputData
+  }
+
+  def getCellGraph(in: String) : Map[Int, Set[Int]] = {
+    val source =Source.fromFile(in)
+    val lines = source.getLines()
+    val graph : Map[Int, Set[Int]] = Map.empty
+    while (lines.hasNext) {
+      val input = lines.next()
+      val inputArray: Array[String] = input.split(" ")
+      for (i <- 1 until inputArray.length) {
+        updateCellGraph(inputArray(0).toInt, inputArray(i).toInt, graph)
+      }
+    }
+    graph
+  }
+
+  def updateCellGraph(key: Int, value: Int, map: Map[Int, Set[Int]]) : Map[Int, Set[Int]] = {
+    map.get(key)
+      .map(_=> map(key) += value)
+      .getOrElse(map(key) = Set[Int](value))
+    map
+  }
+
+  def s(n: Int): Int = n * 4 + 1
+
+  def buildPlayerString(list: List[Cell]): String = {
+    val abstand1 = "         "
+    val abstand = "      "
+    val l = list.slice(0,4)
+    s"""|$abstand${l.mkString(s"${abstand1}")}
+        |""".stripMargin
+  }
+
+  def buildString(list: List[Cell],n: Int): String = {
+    val z = n + 2
+    createStringValues(list,n, 0,z,0)
+  }
+
+  def createStringValues(list: List[Cell],n: Int,O: Int,z: Int,i: Int): String = {
+    val abstand = " "
+    val strecke = s(n)
+    val gapBetween = abstand * 13
+
+    if(i == z) {
+      val sliceBeginU = O
+      val sliceEndU = sliceBeginU + strecke
+      val sliceBeginO = sliceEndU
+      val sliceEndO = sliceBeginO + n - 3
+      val gapLeftU = abstand * 0
+      val gapLeftO = abstand*32
+      val gapBetween = abstand*20
+      createString(list, n, sliceBeginU, sliceEndU, sliceBeginO, sliceEndO, gapLeftO,gapLeftU, gapBetween, z, i)
+
+    } else if(i == 1) {
+
+      val sliceBeginU = O
+      val sliceEndU = sliceBeginU + strecke
+      val sliceBeginO = sliceEndU
+      val sliceEndO = sliceBeginO + n
+      val gapLeftU = abstand * 0
+      val gapLeftO = abstand * 8
+      createString(list, n, sliceBeginU, sliceEndU, sliceBeginO, sliceEndO, gapLeftO,gapLeftU, gapBetween, z, i)
+    }
+    else if(i == 2){
+      val sliceBeginU = O
+      val sliceEndU = sliceBeginU + strecke-4
+      val sliceBeginO = sliceEndU
+      val sliceEndO = sliceBeginO + n-2
+      val gapLeftU = abstand * 8
+      val gapLeftO = abstand * 16
+      val gapBetween = abstand * 29
+      createString(list, n, sliceBeginU, sliceEndU, sliceBeginO, sliceEndO, gapLeftO,gapLeftU, gapBetween, z, i)
+    } else if(i == 3) {
+      val sliceBeginU = O
+      val sliceEndU = sliceBeginU + strecke-8
+      val sliceBeginO = sliceEndU
+      val sliceEndO = sliceBeginO + n-2
+      val gapLeftU = abstand * 16
+      val gapLeftO = abstand * 24
+      val gapBetween = abstand * 13
+      createString(list, n, sliceBeginU, sliceEndU, sliceBeginO, sliceEndO, gapLeftO,gapLeftU, gapBetween, z, i)
+    } else if(i == 4) {
+      val sliceBeginU = O
+      val sliceEndU = sliceBeginU + strecke-12
+      val sliceBeginO = sliceEndU
+      val sliceEndO = sliceBeginO + n-3
+      val gapLeftU = abstand * 24
+      val gapLeftO = abstand * 32
+      val gapBetween = abstand * 13
+      createString(list, n, sliceBeginU, sliceEndU, sliceBeginO, sliceEndO, gapLeftO,gapLeftU, gapBetween, z, i)
+    }
+    else if(i == 5) {
+      val sliceBeginU = O
+      val sliceEndU = sliceBeginU + s(n)
+      val sliceBeginO = sliceEndU
+      val sliceEndO = sliceBeginO + n - 2
+      val gapLeftU = abstand * 0
+      val gapLeftO = abstand * 0
+      val gapBetween = abstand * 61
+      createString(list, n, sliceBeginU, sliceEndU, sliceBeginO, sliceEndO, gapLeftO,gapLeftU, gapBetween, z, i)
+    }
+    else {
+      val gapLeftO = abstand * 0
+      val gapLeftU = abstand * 0
+      val sliceBeginU = n
+      val sliceEndU = sliceBeginU + strecke
+      val sliceBeginO = sliceEndU
+      val sliceEndO = sliceBeginO + n + 1
+      createString(list, n, sliceBeginU, sliceEndU, sliceBeginO, sliceEndO, gapLeftO,gapLeftU, gapBetween, z, i)
+    }
+  }
+
+  def createString(list: List[Cell],n: Int,sliceBeginU: Int, sliceEndU: Int,sliceBeginO: Int, sliceEndO: Int,gapLeftO: String,gapLeftU: String, gapBetween: String, z: Int,i: Int): String = {
+    val ol = list.slice(sliceBeginO,sliceEndO)
+    val ul = list.slice(sliceBeginU,sliceEndU)
+    val ol1 = for(t <- ol)yield t
+    val ul1 = for(t <- ul)yield t
+    if(i == z) {
+      s"""|${gapLeftO}${ol1.mkString(s"${gapBetween}")}
+          |${gapLeftU}${ul1.mkString("-")}
+          |""".stripMargin
+    } else {
+      createStringValues(list,n,sliceEndO,z,i+1) +  s"""|${gapLeftO}${ol1.mkString(s"${gapBetween}")}
+                                                        |${gapLeftU}${ul1.mkString("-")}
+                                                        |""".stripMargin
+    }
+
+  }
+}
