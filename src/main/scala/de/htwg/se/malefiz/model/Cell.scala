3d121088718c33710c04759c4694027b6e5a3fd4
alban26 <alban.jashari@htwg-konstanz.de>
2020-06-03 18:40:42 +0200

    new changes

---
+++ src/main/scala/de/htwg/se/malefiz/model/Cell.scala
@@ -0,0 +1,8 @@
+package de.htwg.se.malefiz.model
+
+case class Cell(cellNumber: Int, playerNumber: Int, destination: Boolean, wallPermission: Boolean, hasWall: Boolean,
+                coordinates: Point) {
+
+  override def toString:String = "[ ]" + cellNumber
+
+}
