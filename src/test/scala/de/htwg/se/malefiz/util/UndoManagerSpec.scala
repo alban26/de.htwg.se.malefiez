9a31be23371726d4a422d7ac4b20cf76de1ea5e9
Robert Forsh <55754729+RF196@users.noreply.github.com>
2020-06-18 21:47:18 +0200

    added CommandSpec and UndoManagerSpec

---
+++ src/test/scala/de/htwg/se/malefiz/util/UndoManagerSpec.scala
@@ -0,0 +1,41 @@
+import de.htwg.se.malefiz.controller.Controller
+import de.htwg.se.malefiz.model.{Cell, Creator, GameBoard, Player}
+import de.htwg.se.malefiz.util.UndoManager
+import org.scalatest.matchers.should.Matchers
+import org.scalatest._
+
+import scala.collection.mutable.Map
+
+
+class UndoManagerSpec extends WordSpec with Matchers {
+
+  "An UndoManager" should {
+    val undoManager = new UndoManager
+
+    "have a do, undo and redo" in {
+      val command = new incrCommand
+      command.state should be(0)
+      undoManager.doStep(command)
+      command.state should be(1)
+      undoManager.undoStep
+      command.state should be(0)
+      undoManager.redoStep
+      command.state should be(1)
+    }
+
+    "handle multiple undo steps correctly" in {
+      val command = new incrCommand
+      command.state should be(0)
+      undoManager.doStep(command)
+      command.state should be(1)
+      undoManager.doStep(command)
+      command.state should be(2)
+      undoManager.undoStep
+      command.state should be(1)
+      undoManager.undoStep
+      command.state should be(0)
+      undoManager.redoStep
+      command.state should be(1)
+    }
+  }
+}
