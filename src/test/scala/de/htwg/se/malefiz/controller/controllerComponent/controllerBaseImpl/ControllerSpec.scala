ba8a721aff4a22d15542e2b47d4adab789b1cb01
Robert Forsh <55754729+RF196@users.noreply.github.com>
2020-07-02 18:43:27 +0200

    added some test

--- src/test/scala/de/htwg/se/malefiz/controller/controllerComponent/controllerBaseImpl/ControllerSpec.scala
+++ src/test/scala/de/htwg/se/malefiz/controller/controllerComponent/controllerBaseImpl/ControllerSpec.scala
@@ -22,36 +22,34 @@ class ControllerSpec  extends WordSpec with Matchers {
       val cellGraph : Map[Int, Set[Int]] = Creator().getCellGraph(cellLinksFile)
 
       val controller = new Controller(GameBoard(cellList, players, cellGraph, Set().empty))
-      //controller.player = List(Player(1, "Robert"), Player(1, "Alban"))
+      controller.gameBoard.createPlayer("Robert")
+      controller.gameBoard.createPlayer("Alban")
       controller.playersTurn = Player(1,"Robert")
 
-      val observer = new Observer {
+      val observer: Observer = new Observer {
         var updated: Boolean = false
         def isUpdated: Boolean = updated
-        override def update: Unit = {updated = true; updated}
+        override def update: Unit = {updated = true}
       }
-
-
-
       "notify its Observer after a players figure is set on cell" in {
         controller.setPlayerFigure(1,1, 10)
-        observer.updated should be(true)
+        observer.update should be()
         controller.getFigure(1,1) should be (10)
       }
       "notify its Observer after setting a Wall on a Cell" in {
         controller.setWall(50)
-        observer.updated should be(true)
+        observer.update should be()
        // controller.gameBoard.cellList(50).hasWall should be (true)
       }
       "notify its Observer after the cube is thrown which cells are possible to go" in {
         controller.getPCells(20, 5)
-        observer.updated should be(true)
+        observer.update should be()
       }
       "Remove Player" in {
         controller.setPlayer(1,22)
         controller.setFigure(1,22)
-       // controller.gameBoard.cellList(22).playerNumber should be(1)
-       // controller.gameBoard.cellList(22).figureNumber should be(1)
+        controller.gameBoard.getCellList(22).playerNumber should be(1)
+        controller.gameBoard.getCellList(22).figureNumber should be(1)
       }
     }
   }
