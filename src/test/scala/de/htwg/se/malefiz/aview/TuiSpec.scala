356a1109b5fe1b37a5b3543ff8ed13aa8aba2204
alban26 <alban.jashari@htwg-konstanz.de>
2020-07-08 12:00:31 +0200

    try tests

--- src/test/scala/de/htwg/se/malefiz/aview/TuiSpec.scala
+++ src/test/scala/de/htwg/se/malefiz/aview/TuiSpec.scala
@@ -1,11 +1,13 @@
 package de.htwg.se.malefiz.aview
 
+import com.google.inject.{Guice, Injector}
+import de.htwg.se.malefiz.MalefizModule
+import de.htwg.se.malefiz.controller.controllerComponent.ControllerInterface
 import org.scalatest.matchers.should.Matchers
 import org.scalatest._
 import de.htwg.se.malefiz.controller.controllerComponent.controllerBaseImpl.Controller
 import de.htwg.se.malefiz.model.gameBoardComponent.gameBoardBaseImpl.{Cell, Creator, GameBoard}
 import de.htwg.se.malefiz.model.playerComponent.Player
-
 
 import scala.collection.mutable.Map
 
@@ -21,14 +23,15 @@ class TuiSpec extends WordSpec with Matchers {
       val cellGraph: Map[Int, Set[Int]] = Creator().getCellGraph(cellLinksFile)
       val possibleCells: Set[Int] = Set().empty
 
-      val controller = new Controller(GameBoard(cellList, players, cellGraph, possibleCells))
+      val injector: Injector = Guice.createInjector(new MalefizModule)
+      val controller: ControllerInterface = injector.getInstance(classOf[ControllerInterface])
       val tui = new Tui(controller)
 
       "read names from the console" in {
         tui.processInput1("n Robert")
         tui.processInput1("n Alban")
 
-        val playerList = controller.gameBoard.getPlayer
+        val playerList = controller.getPlayer
 
         playerList.head.playerNumber should be (1)
         playerList.head.name should be ("Robert")
