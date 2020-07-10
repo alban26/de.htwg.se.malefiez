96cdbdd6537a3cd48a9ff3e6e7d29e2c3f337235
alban26 <alban.jashari@htwg-konstanz.de>
2020-07-10 18:50:35 +0200

    TUI Testing

--- src/main/scala/de/htwg/se/malefiz/controller/controllerComponent/controllerBaseImpl/SetPlayerCommand.scala
+++ src/main/scala/de/htwg/se/malefiz/controller/controllerComponent/controllerBaseImpl/SetPlayerCommand.scala
@@ -1,6 +1,7 @@
 package de.htwg.se.malefiz.controller.controllerComponent.controllerBaseImpl
 
 import de.htwg.se.malefiz.controller.controllerComponent.GameStates.GameState
+import de.htwg.se.malefiz.controller.controllerComponent.State
 import de.htwg.se.malefiz.controller.controllerComponent.Statements.Statements
 import de.htwg.se.malefiz.model.gameBoardComponent.GameboardInterface
 import de.htwg.se.malefiz.model.playerComponent.Player
@@ -12,7 +13,7 @@ class SetPlayerCommand(playerNumber: Int, playerFigure: Int, cellNumber: Int, co
   var mdicedNumber: Int = controller.dicedNumber
   var mplayersTurn: Player = controller.playersTurn
   var mStatementStatus: Statements = controller.statementStatus
-  var mS: GameState = controller.s
+  var mS: State[GameState] = controller.s.state
 
   override def doStep(): Unit = {
     controller.gameBoard = controller.gameBoard.removeActualPlayerAndFigureFromCell(playerNumber, playerFigure)
@@ -40,14 +41,15 @@ class SetPlayerCommand(playerNumber: Int, playerFigure: Int, cellNumber: Int, co
     val new_mStatementStatus = controller.statementStatus
 
     controller.gameBoard = memento
-    controller.dicedNumber = mdicedNumber
-    controller.s.nextState(mS.state)
+    controller.setDicedNumber(mdicedNumber)
+    controller.s.state = mS
+    println("Hallooooooo  "+ mS)
     controller.playersTurn = mplayersTurn
     controller.statementStatus = mStatementStatus
 
     memento = new_memento
     mdicedNumber = new_mDicedNumber
-    mS.nextState(new_mS.state)
+    mS = new_mS.state
     mplayersTurn = new_mPlayersTurn
     mStatementStatus = new_mStatementStatus
   }
